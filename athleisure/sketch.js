c7fd1e2a018ddecdbdfd7bef16380c37175a9fa9
jack of the suburbs <48495597+jackofthesuburbs@users.noreply.github.com>
2020-01-09 16:13:56 +0000

    Update sketch.js

--- athleisure/sketch.js
+++ athleisure/sketch.js
@@ -1,18 +1,23 @@
-// Daniel Shiffman
-// http://codingtra.in
-// http://patreon.com/codingtrain
-// Code for: https://youtu.be/40Me1-yAtTc
+var canvas;
+var h1;
+
+let osc;
+let playing = false;
+var albumart;
 
 // The snake
 var s;
+
 // The scale of the grid
 var scl = 20;
 var cols;
 var rows;
+var button;
+var buttno;
 
 var music;
 
-var fr = 3;
+var fr = 8;
 
 // This is the food location
 var food;
@@ -21,23 +26,48 @@ var musicRate = 1;
 
 function preload() {
   music = loadSound('athleisure.mp3');
+  albumart = loadImage('albumart.jpg');
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  music.play();
+  canvas = createCanvas(500, 500);
+  canvas.position(windowWidth/2 - 250, windowHeight/2 - 250);
+  h1 = createElement('h1', '<iframe style="border: 0; width: 500px; height: 274px;" src="https://bandcamp.com/EmbeddedPlayer/album=4010593746/size=large/bgcol=ffffff/linkcol=0687f5/artwork=none/transparent=true/" seamless><a href="http://jackofthesuburbs.bandcamp.com/album/athleisure">athleisure by jack of the suburbs</a></iframe>');
+  h1.position(windowWidth/2 - 250, windowHeight/2 + 250);
+  
+  //music.play();
   music.playMode('sustain');
   colorMode(HSB, 255);
   noStroke();
   cols = floor(width / scl);
   rows = floor(height / scl);
+  button = createButton("play");
+  button.mousePressed(togglePlaying);
+  button.position(windowWidth/2 - 250, windowHeight/2 - 280);
+  buttno = createButton("stop");
+  buttno.mousePressed(resetPlaying);
+  buttno.position(windowWidth/2 - 205, windowHeight/2 - 280);
 
   s = new Snake();
+  s.xspeed = 0;
   frameRate(fr);
 
   // Pick a food location
   pickLocation();
 }
+
+function togglePlaying() {
+  music.play();
+  s.xspeed = 1; 
+}
+
+function resetPlaying() {
+  music.stop();
+  s = new Snake();
+  s.xspeed = 0;
+  frameRate(fr);
+}
+  
 
 // Pick a food location
 function pickLocation() {
@@ -47,7 +77,9 @@ function pickLocation() {
 
 // Animation loop
 function draw() {
-  background(54);
+  background(albumart);
+  fill(255, 0, 255, 100);
+  rect(0, 0, width, height);
   var len = music.duration();
   
 
@@ -77,6 +109,10 @@ music.playMode('sustain');
   fill(150, 255, 255);
   rect(food.x, food.y, scl, scl);
 }
+
+if (s.death) {
+  music.stop();
+  }
 
 // Moving the snake
 function keyPressed() {
